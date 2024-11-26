e3ea052a3a02f465f1f8e15dbd192f1b20ceaff1
srktheman000 <sohan.kinage000@gmail.com>
2024-11-26 09:29:18 +0530

    feat : containerize application

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,7 @@ import logger from './config/logger'
 const startServer = () => {
     const PORT = Config.PORT
     try {
-        logger.info('Listening on port', { port: PORT }),
-            logger.error('Listening On port', { port: PORT }),
-            app.listen(PORT, () =>
-                logger.info('Listening On port', { port: PORT }),
-            )
+        app.listen(PORT, () => logger.info('Listening On port', { port: PORT }))
     } catch (err) {
         console.error(err)
         process.exit()
