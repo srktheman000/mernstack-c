6c2264fe4b9941e591024736efc96aa75730dc75
srktheman000 <sohan.kinage000@gmail.com>
2024-11-30 22:12:13 +0530

    add unique email

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -9,6 +9,15 @@ export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
+        // check if user exist
+        const user = await this.userRepository.findOne({ where: { email } })
+
+        if (user) {
+            const err = createHttpError(400, 'Email is already exist')
+            throw err
+        }
+
+        // Hashpassword
         const saltRounds = 10
         const hashedPassword = await bcrypt.hash(password, saltRounds)
         try {
