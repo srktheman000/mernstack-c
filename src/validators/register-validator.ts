a2cabd51a4fcebb414b2c69961b6a082ce7857b7
srktheman000 <sohan.kinage000@gmail.com>
2024-12-01 13:34:24 +0530

    add validation for other feilds

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -3,9 +3,34 @@ import { body, checkSchema } from 'express-validator'
 // Validate that the email field is not empty
 export default checkSchema({
     email: {
-        notEmpty: true,
-        errorMessage: 'Eamil is required',
+        notEmpty: {
+            errorMessage: 'Email is required',
+        },
+        isEmail: {
+            errorMessage: 'Email is not valid',
+        },
+        trim: true,
+    },
+    firstName: {
+        notEmpty: {
+            errorMessage: 'First name is required',
+        },
+        trim: true,
+    },
+    lastName: {
+        notEmpty: {
+            errorMessage: 'Last name is required',
+        },
+        trim: true,
+    },
+    password: {
+        notEmpty: {
+            errorMessage: 'Password is required',
+        },
+        isLength: {
+            options: { min: 8 },
+            errorMessage: 'Password must be at least 8 characters long',
+        },
     },
 })
-
 // export default [body('email').notEmpty().withMessage('Email is required')]
