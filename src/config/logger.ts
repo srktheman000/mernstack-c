ae4f5df91d6414e688f1fd35f3f463caf6cea24c
srktheman000 <sohan.kinage000@gmail.com>
2024-12-03 12:22:52 +0530

    Fix : logger issues

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -4,33 +4,25 @@ import { Config } from '.'
 const logger = winston.createLogger({
     level: 'info',
     defaultMeta: { serviceName: 'auth-service' },
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
     transports: [
         new winston.transports.File({
-            level: 'info',
+            level: 'debug',
             dirname: 'logs',
             filename: 'combined.log',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'test',
         }),
         new winston.transports.File({
             level: 'error',
             dirname: 'logs',
             filename: 'error.log',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'test',
         }),
         new winston.transports.Console({
             level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'test',
         }),
     ],
