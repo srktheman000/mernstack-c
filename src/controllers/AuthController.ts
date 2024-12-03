7d20a9ed0c9ec58fae44bee59cf02bac6dad93d9
srktheman000 <sohan.kinage000@gmail.com>
2024-12-03 12:08:19 +0530

    change status code of login endpoint

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -153,7 +153,7 @@ export class AuthController {
                 id: user.id,
             })
 
-            res.status(201).json({ id: user.id })
+            res.json({ id: user.id })
         } catch (err) {
             next(err)
             return
